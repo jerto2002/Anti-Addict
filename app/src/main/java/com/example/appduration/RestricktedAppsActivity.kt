90188c785740541f29594ee3a4c5d57cb2030ddb
Jeroen Tiels <jeroen.tiels@student.odisee.be>
2022-11-17 16:52:26 +0100

    desing restricked app page

--- app/src/main/java/com/example/appduration/RestricktedAppsActivity.kt
+++ app/src/main/java/com/example/appduration/RestricktedAppsActivity.kt
@@ -2,6 +2,8 @@ package com.example.appduration
 
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appduration.databinding.ActivityRestricktedBinding
@@ -15,5 +17,21 @@ class RestricktedAppsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
+        AddUsersToScreen();
+    }
+    public fun AddUsersToScreen() {
+        /*for(user in articles){
+            var inflater = LayoutInflater.from(this).inflate(R.layout.activity_restrickted, null)
+            binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
+        }*/
+        var inflater = LayoutInflater.from(this).inflate(R.layout.showappblock, null)
+        binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
+        inflater = LayoutInflater.from(this).inflate(R.layout.showappblock, null)
+        binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
+        val count = binding.userLinearview.childCount
+        var v: View?
+        for (i in 0 until count) {
+            v = binding.userLinearview.getChildAt(i)
+        }
     }
 }
