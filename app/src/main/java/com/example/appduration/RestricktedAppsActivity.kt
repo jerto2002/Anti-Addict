19f91ebf42b66e7eb42402da6053ff7e83c222cd
Jeroen Tiels <jeroen.tiels@student.odisee.be>
2022-11-17 18:40:35 +0100

    set all installed apps in restricked app page (with filter)

--- app/src/main/java/com/example/appduration/RestricktedAppsActivity.kt
+++ app/src/main/java/com/example/appduration/RestricktedAppsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.appduration
 
+import android.content.Intent
+import android.content.pm.PackageInfo
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appduration.databinding.ActivityRestricktedBinding
@@ -17,21 +20,58 @@ class RestricktedAppsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
-        AddUsersToScreen();
+        testje();
     }
     public fun AddUsersToScreen() {
         /*for(user in articles){
             var inflater = LayoutInflater.from(this).inflate(R.layout.activity_restrickted, null)
             binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
         }*/
-        var inflater = LayoutInflater.from(this).inflate(R.layout.showappblock, null)
-        binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
-        inflater = LayoutInflater.from(this).inflate(R.layout.showappblock, null)
-        binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
+        val packages: MutableList<PackageInfo> = packageManager.getInstalledPackages(0)
+        for(i in 0 until packages.size){
+
+                var inflater = LayoutInflater.from(this).inflate(R.layout.showappblock, null)
+                binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
+
+        }
+
         val count = binding.userLinearview.childCount
         var v: View?
         for (i in 0 until count) {
             v = binding.userLinearview.getChildAt(i)
+            var d = packageManager.getApplicationIcon(packages.get(i).packageName)
+            val icon: ImageView = v.findViewById(R.id.iconapp)
+            icon.setImageDrawable(d)
+
+            val name = d.toString();
+
+        }
+
+    }
+
+    private fun testje(){
+        var packageManager = packageManager
+        val intent = Intent(Intent.ACTION_MAIN, null)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val resolveInfoList = packageManager.queryIntentActivities(intent, 0) //https://stackoverflow.com/questions/10696121/get-icons-of-all-installed-apps-in-android
+        val t = "";
+
+
+        for(i in 0 until resolveInfoList.size){
+
+            var inflater = LayoutInflater.from(this).inflate(R.layout.showappblock, null)
+            binding.userLinearview.addView(inflater, binding.userLinearview.childCount)
+
+        }
+
+        val count = binding.userLinearview.childCount
+        var v: View?
+        for (i in 0 until count) {
+            v = binding.userLinearview.getChildAt(i)
+            //var d = packageManager.getApplicationIcon(resolveInfoList.get(i).resolvePackageName)
+            val icon: ImageView = v.findViewById(R.id.iconapp)
+            icon.setImageDrawable(resolveInfoList.get(i).activityInfo.loadIcon(packageManager))
+           // val name = d.toString();
         }
     }
 }
