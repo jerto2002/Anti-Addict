924c984a480174cc3f4881a37bda1859063537c7
Jeroen Tiels <jeroen.tiels@student.odisee.be>
2022-12-27 15:28:11 +0100

    add firebase

--- app/src/main/java/com/example/appduration/MainActivity.kt
+++ app/src/main/java/com/example/appduration/MainActivity.kt
@@ -36,6 +36,8 @@ import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.Transformer
 import com.github.mikephil.charting.utils.Utils
 import com.github.mikephil.charting.utils.ViewPortHandler
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -77,7 +79,10 @@ class MainActivity : AppCompatActivity() { //order code straks
             while(!checkUsageStatsPermission()){}
             findAppsDurationTimes()
         }
-
+        testfirebase();
+    }
+    fun testfirebase(){
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference();
     }
     fun nav(){
         binding.bottomNavigationView.setOnItemSelectedListener {
